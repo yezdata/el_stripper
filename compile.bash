ba9339a4f29a9656ce75193a30a428eca9d99436
Václav Jež <jez.vasek@gmail.com>
2026-07-11 13:18:20 +0200

    fix gh workflow compilation

--- compile.bash
+++ compile.bash
@@ -3,19 +3,15 @@ set -euo pipefail
 OUTPUT_DIR="dist"
 mkdir -p "$OUTPUT_DIR"
 
-uv venv --isolated --python 3.12
+if [ -f ".venv/Scripts/python.exe" ]; then
+    VENV_PYTHON=".venv/Scripts/python.exe"
+else
+    VENV_PYTHON=".venv/bin/python"
+fi
 
-source ".venv/bin/activate"
-
-uv pip install \
-    "Nuitka[onefile]" \
-    "libcst" \
-    "pathspec" \
-
-python -m nuitka \
+"$VENV_PYTHON" -m nuitka \
     --standalone \
     --onefile \
     --enable-plugin=anti-bloat \
     --output-dir="$OUTPUT_DIR" \
-    --report=report.xml \
     el_stripper.py
